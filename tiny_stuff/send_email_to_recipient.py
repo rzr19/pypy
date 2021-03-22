974c700b645001c60a4a3e7ba483ab911f7b2068
rzr19 <gheorghexvlad@gmail.com>
2021-03-22 20:53:52 +0200

    Create send_email_to_recipient.py

---
+++ tiny_stuff/send_email_to_recipient.py
@@ -0,0 +1,28 @@
+import smtplib
+
+DEFAULT_EMAIL_ADDRESS = None
+
+def sendemail( message, subject, to_addr = None ):
+    """ Simple function to send emails. Default server used is the localhost,
+        so there should be a simple SMTP-able MTA agent on the server where you runt this.
+        :param message: The body of the message.
+        :param subject: The subject of the message.
+        :param to_addr: The destination email address.
+    """
+
+    if (to_addr is None) or (not isinstance( to_addr, (str, unicode) ) ):
+        to_addr = DEFAULT_EMAIL_ADDRESS
+
+    sender = 'health_check@gitlab.app.betfair'
+    receivers = [ to_addr ]
+    message = ("From: %s\n" % sender) + \
+              ("To: %s\n" % to_addr) + \
+              ("Subject: %s\n\n" % subject ) + \
+              message
+
+    try:
+        smtpObj = smtplib.SMTP('localhost')
+        smtpObj.sendmail(sender, receivers, message)
+        print( "Successfully sent email to '%s'" % to_addr )
+    except:
+        print( "Error: unable to send email to '%s'" % to_addr )
